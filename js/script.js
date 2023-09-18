ec085563f4ff1f6d5f0430ebbe9f32b823e36c2e
Igor Xavier <igorxrds@gmail.com>
2023-09-17 21:27:34 -0300

    add file

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+function menuSuspenso() {
+    var element = document.getElementById("menuLinks");
+    element.classList.toggle("show");
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header ul li a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header ul li a[href*=' + id + ']').classList.add
+                ('active');
+            })
+        }
+    })
+}
+
+
