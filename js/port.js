490de876028149521cf97cc52492083fd509021f
Igor Xavier <igorxrds@gmail.com>
2023-09-19 22:08:42 -0300

    Atualizaçoes

--- js/port.js
+++ js/port.js
@@ -1,36 +1,35 @@
 
-  //-------------------------------------------
-  function zomProjeto(element) {
-    element.classList.toggle("activep");
-    const botoesDiv = element.querySelector(".botoes");
-    botoesDiv.classList.toggle("activep");
-}
+  //------------------Igor Xaiver-------------------------
 
-
-function removerClasseActivep() {
-    const elementosComActivep = document.querySelectorAll(".activep");
-
-    elementosComActivep.forEach((elemento) => {
-        elemento.classList.remove("activep");
-    });
-}
-
-const projetos = document.querySelectorAll(".box");
-projetos.forEach((projeto) => {
-    projeto.addEventListener("click", zomProjeto);
-});
-
+// Esta função é chamada quando uma div .box é clicada.
 function zomProjeto(element) {
+    // Toggle (adiciona ou remove) a classe "activep" na div que foi clicada.
     element.classList.toggle("activep");
-    const boxes = document.querySelectorAll(".box");
-    const blur = document.getElementById("blur");
-    const atLeastOneActive = [...boxes].some(box => box.classList.contains("activep"));
-
-    if (atLeastOneActive) {
-        blur.classList.add("activep");
-    } else {
-        blur.classList.remove("activep");
-    }
-}
-
-
+  
+    // Seleciona a div .blur e adiciona ou remove a classe "activep" nela.
+    const blur = document.querySelector('.blur');
+    blur.classList.toggle('activep');
+  
+    // Seleciona o elemento .botoes dentro da div que foi clicada e adiciona ou remove a classe "activep" nele.
+    const botoesDiv = element.querySelector(".discricao-projetos");
+    botoesDiv.classList.toggle("activep");
+  }
+  
+  // Esta função é chamada para remover a classe "activep" de todos os elementos que a possuem.
+  function removerClasseActivep() {
+    // Seleciona todos os elementos que possuem a classe "activep".
+    const elementosComActivep = document.querySelectorAll(".activep");
+  
+    // Itera sobre esses elementos e remove a classe "activep" de cada um deles.
+    elementosComActivep.forEach((elemento) => {
+      elemento.classList.remove("activep");
+    });
+  }
+  
+  // Seleciona todas as divs com a classe .box e adiciona um ouvinte de eventos de clique a cada uma delas.
+  const projetos = document.querySelectorAll(".box");
+  projetos.forEach((projeto) => {
+    // Quando uma div .box é clicada, a função zomProjeto será chamada.
+    projeto.addEventListener("click", zomProjeto);
+  });
+  
--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
-
+// Função para mostrar ou ocultar o menu suspenso
 function menuSuspenso() {
+    // Seleciona o elemento com o id "menuLinks"
     var element = document.getElementById("menuLinks");
+
+    // Alterna (adiciona ou remove) a classe "show" no elemento selecionado
     element.classList.toggle("show");
-    var element = document.getElementById("header");
-    element.classList.toggle("show2");
+
+    // Seleciona o elemento com o id "header"
+    var element2 = document.getElementById("header");
+
+    // Alterna (adiciona ou remove) a classe "show2" no segundo elemento selecionado
+    element2.classList.toggle("show2");
 }
 
+// Seleciona todas as seções e os links do menu de navegação
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header ul li a')
 
+// Evento que é acionado quando a página é rolada
 window.onscroll = () => {
     sections.forEach(sec => {
+        // Obtém a posição atual da rolagem da página
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header ul li a[href*=' + id + ']').classList.add
-                ('active');
+        // Verifica se a posição atual está dentro dos limites da seção atual
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                // Remove a classe 'active' de todos os links do menu
+                link.classList.remove('active');
+
+                // Adiciona a classe 'active' ao link correspondente à seção atual
+                document.querySelector('header ul li a[href*=' + id + ']').classList.add('active');
             })
         }
     })
 }
-
-
